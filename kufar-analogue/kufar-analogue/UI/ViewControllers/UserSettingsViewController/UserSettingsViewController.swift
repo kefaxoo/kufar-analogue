727b775673f4c74967ba70ca847bccfdd4c01b4f
maxik86 <107717903+maxik86@users.noreply.github.com>
2023-04-17 14:08:47 +0300

    a?

--- kufar-analogue/kufar-analogue/UI/ViewControllers/UserSettingsViewController/UserSettingsViewController.swift
+++ kufar-analogue/kufar-analogue/UI/ViewControllers/UserSettingsViewController/UserSettingsViewController.swift
@@ -7,9 +7,10 @@
 
 import UIKit
 import FirebaseAuth
+import SPIndicator
 
 class UserSettingsViewController: UIViewController {
-
+    
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
@@ -29,6 +30,9 @@ class UserSettingsViewController: UIViewController {
         } else {
             // TODO: handle error with the help of SPIndicator
             // and dismiss
+            SPIndicator.present(title: "Please login🥹",message: "Log in to change your profile", preset: .error, haptic: .error, from: .top)
+            navigationController?.popViewController(animated: true);
+            
         }
     }
     
@@ -39,7 +43,7 @@ class UserSettingsViewController: UIViewController {
         else { return }
         
         let alertVC = UIAlertController(title: "Save changes?", message: "Are you sure to make changes?", preferredStyle: .alert)
-        let yesAction = UIAlertAction(title: "ХЛЭБ", style: .default) { _ in
+        let yesAction = UIAlertAction(title: "YES", style: .default) { _ in
             if var user = Auth.auth().currentUser {
                 let changeProfireRequest = user.createProfileChangeRequest()
                 changeProfireRequest.displayName = name
@@ -48,11 +52,14 @@ class UserSettingsViewController: UIViewController {
                     user.updateEmail(to: email) { error in
                         if let error {
                             // TODO: handle error (SPIndicator)
+                            SPIndicator.present(title: "Error😡",message: "Error when changing email", preset: .error, haptic: .error, from: .top)
                         } else {
                             // TODO: handle verify notification
+                            SPIndicator.present(title: "Email update", preset: .done, haptic: .error, from: .top)
                         }
                     }
                 }
+            
                 
                 if password.count > 5 {
                     user.updatePassword(to: password)
@@ -60,19 +67,24 @@ class UserSettingsViewController: UIViewController {
                 
                 changeProfireRequest.commitChanges { error in
                     // TODO: the same as 55-59 but in else block we sent notification, that all is goooood in any way dismiss
+                    if let error {
+                        SPIndicator.present(title: "Short password",message: "password must be greater that 5", preset: .error, haptic: .error, from: .top)
+                    } else {
+                        // TODO: handle error with the help of SPIndicator
+                        // and dismiss
+                        SPIndicator.present(title: "Password update", preset: .done, haptic: .error, from: .top)
+                    }
                 }
-            } else {
-                // TODO: handle error with the help of SPIndicator
-                // and dismiss
             }
+                alertVC.addAction(yesAction)
+                let noAction = UIAlertAction(title: "NO", style: .destructive) { _ in
+                    self.dismiss(animated: true)
+                }
+                
+                alertVC.addAction(noAction)
+                self.present(alertVC, animated: true)
+            }
+            
         }
-        
-        alertVC.addAction(yesAction)
-        let noAction = UIAlertAction(title: "МАТЧА", style: .destructive) { _ in
-            self.dismiss(animated: true)
-        }
-        
-        alertVC.addAction(noAction)
-        self.present(alertVC, animated: true)
     }
-}
+
